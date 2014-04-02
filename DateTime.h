9a6c1ccd16b4c28417245ca7703ceed00fac22dc
Mamat Rahmat <coderbodoh@gmail.com>
2014-04-02 17:27:36 +0700

    Doxy

--- DateTime.h
+++ DateTime.h
@@ -8,18 +8,116 @@ using namespace std;
 
 class DateTime{
 public:
+    /**
+     * @relates DateTime
+     * @brief Konstruktor tanpa parameter
+     * @details Instansiasi Objek dengan nilai awal
+     */
     DateTime();
+
+    /**
+     * @relates DateTime
+     * @brief Konstruktor dengan parameter
+     * @details Konstruktor dengan parameter
+     * 
+     * @param e Tanggal dari Objek DateTime yang akan diinstansikan
+     * @param e Waktu dari Objek DateTime yang akan diinstansikan
+     */
     DateTime(Date, Time);
+
+    /**
+     * @relates DateTime
+     * @brief Copy Konstruktor
+     * @details Copy Konstruktor
+     * 
+     * @param DT Objek yang akan diinstansiasikan ke dalam konstruktor
+     */
     DateTime(const DateTime& DT);
+
+    /**
+     * @relates DateTime
+     * @brief Operator Overloading =
+     * @details Penginisialisasian Objek
+     * 
+     * @param d objek DateTime yang akan dimasukkan
+     */
     DateTime& operator=(const DateTime& d);
+
+    /**
+     * @relates DateTime
+     * @brief Operator overloading >
+     * @details Membandingkan dua objek DateTime apakah yang kiri lebih besar dari yang kanan
+     * 
+     * @param d objek DateTime
+     * 
+     * @return boolean berisi nilai apakah objek DateTime di kiri lebih besar dari yang kanan
+     */
 	bool operator>(const DateTime& d);
+
+    /**
+     * @relates DateTime
+     * @brief Operator overloading ==
+     * @details Membandingkan dua objek DateTime apakah yang kiri sama dengan yang kanan
+     * 
+     * @param d objek DateTime
+     * 
+     * @return boolean berisi nilai apakah objek DateTime di kiri sama dengan yang kanan
+     */
     bool operator==(const DateTime& d);
+
+    /**
+     * @relates DateTime
+     * @brief Operator overloading <<
+     * @details Prosedur yang memungkinkan objek event untuk melakukan output secara langsung
+     * 
+     * @param output stream output
+     * @param d DateTime yang akan dioutputkan ke stream
+     */
 	friend ostream &operator<<(ostream &output,const DateTime &d);
+
+    /**
+     * @brief Operator overloading <<
+     * @details Prosedur yang memungkinkan objek event untuk menerima input secara langsung
+     * 
+     * @param input stream input
+     * @param d DateTime yang akan diinputkan ke stream
+     */
 	friend istream &operator>>(istream &input,DateTime &d);
+
+    /**
+     * @brief Destruktor
+     * @details Destruktor
+     */
     ~DateTime();
+
+    /**
+     * @brief Setter Date
+     * @details Prosedur yang mengeset nilai Time
+     * 
+     * @param _time Time yang akan dijadikan nilai waktu dari DateTime ini
+     */
     void setTime(Time _time);
+
+    /**
+     * @brief Setter Date
+     * @details Prosedur yang mengeset nilai Date
+     * 
+     * @param _date Date yang akan dijadikan nilai tanggal dari DateTime ini
+     */
     void setDate(Date _date);
+
+    /**
+     * @brief Getter Time
+     * @details Fungsi yang mengembalikan attribute Time dari event ini
+     * @return Date dari DateTime ini
+     */
     Time getTime();
+
+    /**
+     * @brief Getter Date
+     * @details Fungsi yang mengembalikan attribute Date dari event ini
+     * @return Time dari DateTime ini
+     */
 	Date getDate();
 
 private:
--- Event.h
+++ Event.h
@@ -10,18 +10,111 @@ using namespace std;
 
 class Event{
 public:
+	/**
+	 * @relates Event
+	 * @brief Konstruktor tanpa parameter
+	 * @details Instansiasi Objek dengan nilai awal
+	 */
 	Event();
+
+	/**
+	 * @relates Event
+	 * @brief Kostruktor dengan parameter
+	 * @details Kostruktor dengan parameter
+	 * 
+	 * @param e Waktu dari Event yang akan diinstansikan
+	 * @param r Kode dari Event yang akan diinstansikan
+	 * @param t ID dari Event yang akan diinstansikan
+	 */
 	Event(DateTime, char, int);
+
+	/**
+	 * @relates Event
+	 * @brief Kostruktor dengan parameter
+	 * @details Kostruktor dengan parameter
+	 * 
+	 * @param E Objek yang akan diinstansiasikan ke dalam konstruktor
+	 */
 	Event(const Event& E);
+
+	/**
+	 * @relates Event
+	 * @brief operator overloading =
+	 * @details Penginisialisasian Objek
+	 * 
+	 * @param E Event yang akan dimasukkan
+	 */
 	Event& operator=(const Event& E);
+
+	/**
+	 * @relates Event
+	 * @brief Destruktor
+	 * @details Desktruktor
+	 */
 	~Event();
+
+	/**
+	 * @relates Event
+	 * @brief Operator Overloading >>
+	 * @details Prosedur yang memungkinkan objek event untuk melakukan output secara langsung
+	 * 
+	 * @param output stream output
+	 * @param e Event yang akan dioutputkan ke stream
+	 */
 	friend ostream &operator<<(ostream &output,const Event &e);
+
+	/**
+	 * @brief Operator Overloading >>
+	 * @details Prosedur yang memungkinkan objek event untuk menerima input secara langsung
+	 * 
+	 * @param input stream input
+	 * @param e Event yang akan diinputkan ke stream
+	 */
 	friend istream &operator>>(istream &input, Event &e);
+
+	/**
+	 * @brief Setter waktu
+	 * @details Fungsi yang mengeset nilai waktu
+	 * 
+	 * @param  Waktu yang akan dijadikan nilai waktu terjadinya event ini
+	 */
 	void setDateTime(DateTime);
+
+	/**
+	 * @brief Setter kode
+	 * @details Prosedur yang mengeset nilai kode
+	 * 
+	 * @param Kode yang akan dijadikan nilai kode dari event ini
+	 */
 	void setKode(char);
+
+	/**
+	 * @brief Setter ID
+	 * @details Prosedur yang mengeset nilai ID
+	 * 
+	 * @param ID yang akan dijadikan nilai ID dari event ini
+	 */
 	void setId(int);
+
+	/**
+	 * @brief Getter Waktu
+	 * @details Fungsi yang mengembalikan attribute Waktus dari event ini
+	 * @return Waktu terjadi event ini
+	 */
 	DateTime getDateTime();
+
+	/**
+	 * @brief Getter Kode
+	 * @details Fungsi yang mengembalikan attribute kode dari event ini
+	 * @return Kode dari event ini
+	 */
 	char getKode();
+
+	/**
+	 * @brief Getter ID
+	 * @details Fungsi yang mengembalikan attribute ID dari event ini
+	 * @return ID dari event ini
+	 */
 	int  getId();
 	
 private:
