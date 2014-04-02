47f08c317654880c987fa4c1c2e75d6a95ff618b
Mamat Rahmat <coderbodoh@gmail.com>
2014-04-02 08:08:07 +0700

    v2

--- mBank.cpp
+++ mBank.cpp
@@ -1,7 +1,34 @@
+#include <iostream>
 #include "Bank.h"
 
-int main()
-{
-	Bank B;
+using namespace std;
+
+int main(){
+	
+	Queue *myQueue = new Queue[3];
+	myQueue[0].push(1);
+	myQueue[0].push(2);
+	myQueue[0].push(3);
+	myQueue[1].push(3);
+	myQueue[1].push(2);
+	myQueue[1].push(1);
+	myQueue[0].Print();
+	myQueue[1].Print();
+	DateTime _datetime;
+	int n;
+	cin >> _datetime;
+	cin >> n;
+	Bank myBank(_datetime,n);
+	cout << myBank.getN();
+	cout << myBank.getDateTime();
+	cout << endl;
+	myBank.Push(0,1);
+	myBank.Push(0,2);
+	myBank.Push(0,3);
+	myBank.Push(1,11);
+	myBank.Push(1,12);
+	myBank.Push(1,13);
+	myBank.getQueue(0).Print();
+	myBank.getQueue(1).Print();
 	return 0;
 }
